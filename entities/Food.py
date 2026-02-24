de0a34d776e6a9a14fb448eb9593ba002000af8c
Rachel Hartfelder <raehartfelder@gmail.com>
2026-02-23 22:16:14 -0800

    creatures and food finally working

--- entities/Food.py
+++ entities/Food.py
@@ -11,10 +11,23 @@ class Food:
         self.color = (92, 169, 4)  # Green
         self.energy = ENERGY_DENSITY * math.pi * self.radius ** 2
 
+        self.sprites = []
+
+        self.sprites.append(pygame.image.load("Assets/Images/apple.png").convert_alpha())
+
+        self.image = self.sprites[0].subsurface(self.sprites[0].get_bounding_rect())
+
+        self.image = pygame.transform.scale(self.image, (radius * 2, radius * 2))
+
+  
+
     def is_alive(self):
         return self.energy > 0
 
     def draw(self, screen, camera):
         screen_pos = camera.world_to_screen((self.pos.x, self.pos.y))
         scaled_radius = self.radius * camera.zoom
-        pygame.draw.circle(screen, self.color, (int(screen_pos[0]), int(screen_pos[1])), int(scaled_radius))
+
+        self.image_scaled = pygame.transform.scale_by(self.image, camera.zoom)
+        screen.blit(self.image_scaled, (screen_pos[0], screen_pos[1]))
+        #pygame.draw.circle(screen, self.color, (int(screen_pos[0]), int(screen_pos[1])), int(scaled_radius))
