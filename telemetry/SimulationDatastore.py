6dd8afc25ad4331da1ad461cb9966b67b8805353
logancostello <locostel@calpoly.edu>
2026-03-06 15:03:27 -0800

    add: data saved every 15 simulation minutes

--- telemetry/SimulationDatastore.py
+++ telemetry/SimulationDatastore.py
@@ -2,17 +2,16 @@ import sqlite3
 import pandas as pd
 import os
 
+AUTOSAVE_INTERVAL = 15 * 60  # save every 15 simulation minutes
 
 class SimulationDatastore:
-    """ Place for simulation data to be stored """
-
     def __init__(self):
         self.conn = sqlite3.connect(":memory:")
         self.create_tables()
+        self._last_save_time = 0
 
     def create_tables(self):
         cursor = self.conn.cursor()
-
         cursor.execute("""
             CREATE TABLE creatures (
                 id INTEGER PRIMARY KEY,
@@ -32,7 +31,6 @@ class SimulationDatastore:
                 num_brain_connections INTEGER
             )
         """)
-
         cursor.execute("""
             CREATE TABLE real_time_stats (
                 time REAL PRIMARY KEY,
@@ -60,28 +58,30 @@ class SimulationDatastore:
                 "fov": c.genome.fov,
                 "num_brain_nodes": c.num_brain_nodes,
                 "num_brain_connections": c.num_brain_connections
-        })
+            })
 
     def mark_creature_dead(self, creature_id, time):
         self.conn.execute(
-            """ UPDATE creatures SET death_time = ? WHERE id = ? """,
+            "UPDATE creatures SET death_time = ? WHERE id = ?",
             (time, creature_id)
         )
 
     def update_real_time(self, time, num_creatures, num_food):
         self.conn.execute(
-            """ INSERT INTO real_time_stats VALUES (?, ?, ?) """,
+            "INSERT INTO real_time_stats VALUES (?, ?, ?)",
             (time, num_creatures, num_food)
         )
+        self._autosave(time)
+
+    def _autosave(self, time):
+        if time - self._last_save_time >= AUTOSAVE_INTERVAL:
+            self.save()
+            self._last_save_time = time
 
     def save(self):
-        os.makedirs("data", exist_ok=True)  
-
-        creatures_df = pd.read_sql("SELECT * FROM creatures", self.conn)
-        creatures_df.to_csv("data/creatures.csv")
-
-        real_time_stats_df = pd.read_sql("SELECT * FROM real_time_stats", self.conn)
-        real_time_stats_df.to_csv("data/real_time_stats.csv")
+        os.makedirs("data", exist_ok=True)
+        pd.read_sql("SELECT * FROM creatures", self.conn).to_csv("data/creatures.csv")
+        pd.read_sql("SELECT * FROM real_time_stats", self.conn).to_csv("data/real_time_stats.csv")
 
     def close(self):
         self.save()
